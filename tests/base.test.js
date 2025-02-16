bf0814549bde4b64f3be2fad4b608ef84087d150
QANSIGLIERE <qansigliere@gmail.com>
2025-02-16 18:42:06 +0300

    Fixed the issue

--- tests/base.test.js
+++ tests/base.test.js
@@ -82,3 +82,40 @@ test('Parse gitlab valid headers', () => {
         next: 'https://gitlab.com/api/v4/users?active=false&admins=false&auditors=false&blocked=false&exclude_active=false&exclude_external=false&exclude_humans=false&exclude_internal=false&external=false&humans=false&order_by=id&page=2&per_page=20&skip_ldap=false&sort=desc&with_custom_attributes=false&without_project_bots=false&without_projects=false',
     });
 });
+
+test('Fixed bug', () => {
+    let headers = {
+        date: 'Sun, 16 Feb 2025 15:37:33 GMT',
+        'content-type': 'application/json; charset=utf-8',
+        'cache-control': 'private, max-age=60, s-maxage=60',
+        vary: 'Accept, Authorization, Cookie, X-GitHub-OTP,Accept-Encoding, Accept, X-Requested-With',
+        etag: 'W/"444f830838f7afe42d388ad25ee6b7bd900ea021e8f94efed012f1445e29dc80"',
+        'github-authentication-token-expiration': '2025-03-16 17:35:17 +0300',
+        'x-github-media-type': 'github.v3; format=json',
+        link: '<https://api.github.com/repositories/929484816/pulls?state=all&per_page=1&page=2>; rel="next", <https://api.github.com/repositories/929484816/pulls?state=all&per_page=1&page=2>; rel="last"',
+        'x-accepted-github-permissions': 'pull_requests=read',
+        'x-github-api-version-selected': '2022-11-28',
+        'x-ratelimit-limit': '5000',
+        'x-ratelimit-remaining': '4970',
+        'x-ratelimit-reset': '1739723695',
+        'x-ratelimit-used': '30',
+        'x-ratelimit-resource': 'core',
+        'access-control-expose-headers':
+            'ETag, Link, Location, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Used, X-RateLimit-Resource, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval, X-GitHub-Media-Type, X-GitHub-SSO, X-GitHub-Request-Id, Deprecation, Sunset',
+        'access-control-allow-origin': '*',
+        'strict-transport-security': 'max-age=31536000; includeSubdomains; preload',
+        'x-frame-options': 'deny',
+        'x-content-type-options': 'nosniff',
+        'x-xss-protection': '0',
+        'referrer-policy': 'origin-when-cross-origin, strict-origin-when-cross-origin',
+        'content-security-policy': "default-src 'none'",
+        'transfer-encoding': 'chunked',
+        server: 'github.com',
+        'x-github-request-id': '7F30:15CAD0:F340F4:FC9883:67B2063D',
+    };
+
+    expect(linkParser(headers)).toEqual({
+        last: 'https://api.github.com/repositories/929484816/pulls?state=all&per_page=1&page=2',
+        next: 'https://api.github.com/repositories/929484816/pulls?state=all&per_page=1&page=2',
+    });
+});
