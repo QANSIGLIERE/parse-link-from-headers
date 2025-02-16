70b576290760ccfb8de0f33ca617c38ef3ba8a0d
QANSIGLIERE <qansigliere@gmail.com>
2025-02-16 18:08:10 +0300

    JS-226

--- tests/base.test.js
+++ tests/base.test.js
@@ -1,6 +1,6 @@
 var { linkParser } = require('../index');
 
-test('Parse valid headers', () => {
+test('Parse github valid headers', () => {
     let initialData = {
         date: 'Fri, 14 Feb 2025 14:40:38 GMT',
         'content-type': 'application/json; charset=utf-8',
@@ -40,4 +40,45 @@ test('Parse valid headers', () => {
 test('Parse invalid headers', () => {
     let initialData;
     expect(linkParser(initialData)).toBeFalsy();
+});
+
+test('Parse gitlab valid headers', () => {
+    let initialData = {
+        date: 'Fri, 14 Feb 2025 14:58:27 GMT',
+        'content-type': 'application/json',
+        'transfer-encoding': 'chunked',
+        connection: 'keep-alive',
+        'cf-ray': '911dea94cefbd5eb-IST',
+        'cf-cache-status': 'MISS',
+        'cache-control': 'max-age=0, private, must-revalidate',
+        etag: 'W/"c3fabdd15d3393f037e694ed2a11cbc8"',
+        link: '<https://gitlab.com/api/v4/users?active=false&admins=false&auditors=false&blocked=false&exclude_active=false&exclude_external=false&exclude_humans=false&exclude_internal=false&external=false&humans=false&order_by=id&page=2&per_page=20&skip_ldap=false&sort=desc&with_custom_attributes=false&without_project_bots=false&without_projects=false >; rel="next", <https://gitlab.com/api/v4/users?active=false&admins=false&auditors=false&blocked=false&exclude_active=false&exclude_external=false&exclude_humans=false&exclude_internal=false&external=false&humans=false&order_by=id&page=1&per_page=20&skip_ldap=false&sort=desc&with_custom_attributes=false&without_project_bots=false&without_projects=false >; rel="first"',
+        'strict-transport-security': 'max-age=31536000',
+        vary: 'Origin, Accept-Encoding',
+        'content-security-policy': "default-src 'none'",
+        'gitlab-lb': 'haproxy-main-26-lb-gprd',
+        'gitlab-sv': 'api-gke-us-east1-d',
+        'referrer-policy': 'strict-origin-when-cross-origin',
+        'x-content-type-options': 'nosniff',
+        'x-frame-options': 'SAMEORIGIN',
+        'x-gitlab-meta': '{"correlation_id":"23f551df4778aa936e09eeda5e9ca937","version":"1"}',
+        'x-next-page': '2',
+        'x-page': '1',
+        'x-per-page': '20',
+        'x-prev-page': '',
+        'x-request-id': '23f551df4778aa936e09eeda5e9ca937',
+        'x-runtime': '0.195569',
+        'report-to':
+            '{"endpoints":[{"url":"https:\\/\\/a.nel.cloudflare.com\\/report\\/v4?s=srBq7ZMGuV%2FjrjJlnHnuWnTH1EdJGqv6i6Yfslo6rKwruI1iUS3q7MTORlzk4DuaePk%2FcV2DAP0NAm%2FTiq5x3De220AMsTDv%2BOIoEQJUks60qOnf1BXvsdTaOoY%3D"}],"group":"cf-nel","max_age":604800}',
+        nel: '{"success_fraction":0.01,"report_to":"cf-nel","max_age":604800}',
+        'set-cookie': [
+            '_cfuvid=rluxZnemJ0gsaLc_KuAArpkGxf7FmSLK.xktd5FKF2A-1739545107057-0.0.1.1-604800000; path=/; domain=.gitlab.com; HttpOnly; Secure; SameSite=None',
+        ],
+        server: 'cloudflare',
+    };
+
+    expect(linkParser(initialData)).toEqual({
+        first: 'https://gitlab.com/api/v4/users?active=false&admins=false&auditors=false&blocked=false&exclude_active=false&exclude_external=false&exclude_humans=false&exclude_internal=false&external=false&humans=false&order_by=id&page=1&per_page=20&skip_ldap=false&sort=desc&with_custom_attributes=false&without_project_bots=false&without_projects=false',
+        next: 'https://gitlab.com/api/v4/users?active=false&admins=false&auditors=false&blocked=false&exclude_active=false&exclude_external=false&exclude_humans=false&exclude_internal=false&external=false&humans=false&order_by=id&page=2&per_page=20&skip_ldap=false&sort=desc&with_custom_attributes=false&without_project_bots=false&without_projects=false',
+    });
 });
